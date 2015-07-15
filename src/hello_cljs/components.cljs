70ef8faa597873fe039bac050e8ac86df9203e32
david dinh <david.dinh@lookout.com>
2015-07-15 11:42:44 -0700

    WIP

--- src/hello_cljs/components.cljs
+++ src/hello_cljs/components.cljs
@@ -1,7 +1,9 @@
 (ns hello-cljs.components
   (:require
     [om.core :as om :include-macros true]
-    [sablono.core :refer-macros [html]]))
+    [cljs.core.async :as async :refer [put! chan <!]]
+    [sablono.core :refer-macros [html]])
+  (:require-macros [cljs.core.async.macros :as am :refer [go]]))
 
 (defn like-seymore [data owner]
   (reify
@@ -35,4 +37,51 @@
        ;; until it works through the entire list.
        ;; https://clojuredocs.org/clojure.core/cycle#example-557b10f6e4b01ad59b65f4f2
        (map stripe (:list data) (cycle ["#ff0" "#f00"]))])))
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last "," first (middle-name contact)))
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRenderState
+    (render-state [_ {:keys [delete]}]
+      (html
+        [:li
+         [:span (display-name contact)]
+         [:button {:on-click #(put! delete @contact)}"Delete"]]))))
+
+(defn contacts-view [data owner]
+  (reify
+    om/IInitState
+      (init-state [_]
+        {:delete (chan)})
+
+    om/IWillMount
+    (will-mount [_]
+      (let [delete (om/get-state owner :delete)]
+        (go (loop []
+          (let [contact (<! delete)]
+            (om/transact! data :contacts
+              ;; NOTE: vec is used to transform result of `remove`
+              ;; which returns a lazy seq back into a vector
+              (fn [xs] (vec (remove #(= contact %) xs))))
+            (recur))))))
+
+    om/IRenderState
+    (render-state [_ {:keys [delete]}]
+      (html
+        [:div
+          [:h1 "Contact List"]
+          [:ul
+            (om/build-all contact-view
+                          (:contacts data)
+                          {:init-state {:delete delete}})]]))))
+
+
+
 
--- src/hello_cljs/core.cljs
+++ src/hello_cljs/core.cljs
@@ -1,7 +1,8 @@
 (ns hello-cljs.core
     (:require
       [om.core :as om :include-macros true]
-      [hello-cljs.components :refer [like-seymore my-widget animals]]
+      [hello-cljs.components
+        :refer [like-seymore my-widget animals contacts-view]]
       [sablono.core :refer-macros [html]])
     (:require-macros
       [hello-cljs.utils :refer [logger]]))
@@ -12,9 +13,14 @@
 (defonce app-state
   (atom
     {:likes 0
-     :list ["lion" "zebra" "buffalo" "antelope" "gazelle"]}))
-
-(prn @app-state)
+     :contacts
+        [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+         {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+         {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+         {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+         {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+         {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}
+     :list ["lion" "zebra" "buffalo" "antelope" "gazelle"]))
 
 (defn render! []
   (om/root
@@ -24,6 +30,7 @@
         (render [_]
           (html
             [:div.aside-left-menu
+              (om/build contacts-view data)
               (om/build animals data)
               (om/build my-widget data)
               (om/build like-seymore data)]))))
@@ -32,7 +39,5 @@
 
 (render!)
 
-(defn on-js-reload []
-  (render!)
-)
+(defn on-js-reload [] (render!))
 
