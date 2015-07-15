ca66c95478292cd437f80f5895e708fa74afe002
david dinh <david.dinh@lookout.com>
2015-07-15 16:43:33 -0700

    WIP

--- src/hello_cljs/components.cljs
+++ src/hello_cljs/components.cljs
@@ -1,9 +1,13 @@
 (ns hello-cljs.components
   (:require
+    [clojure.string :as str]
+    [clojure.data :as data]
     [om.core :as om :include-macros true]
     [cljs.core.async :as async :refer [put! chan <!]]
     [sablono.core :refer-macros [html]])
   (:require-macros [cljs.core.async.macros :as am :refer [go]]))
+
+;; Misc views
 
 (defn like-seymore [data owner]
   (reify
@@ -38,6 +42,22 @@
        ;; https://clojuredocs.org/clojure.core/cycle#example-557b10f6e4b01ad59b65f4f2
        (map stripe (:list data) (cycle ["#ff0" "#f00"]))])))
 
+;; Contacts View
+
+;; TODO
+;; print message when user has not inputed both first and last
+;; clear input after entry
+
+(defn parse-contact [contact-str]
+  (let [[first middle last :as parts] (str/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (str/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+        (== c 1) (assoc :middle-initial middle)
+        (>= c 2) (assoc :middle middle)))))
+
 (defn middle-name [{:keys [middle middle-initial]}]
   (cond
     middle (str " " middle)
@@ -46,6 +66,22 @@
 (defn display-name [{:keys [first last] :as contact}]
   (str last "," first (middle-name contact)))
 
+(defmulti entry-view (fn [person _] (:type person)))
+
+(defmethod entry-view :student
+  [person owner] (student-view person owner))
+
+(defmethod entry-view :professor
+  [person owner] (professor-view person owner))
+
+(defn registry-view [data owner]
+  (reify
+    om/IRenderState
+    (render-state [_ state]
+      (html
+        [:div
+         [:h1 "Registry"]]))))
+
 (defn contact-view [contact owner]
   (reify
     om/IRenderState
@@ -53,14 +89,28 @@
       (html
         [:li
          [:span (display-name contact)]
-         [:button {:on-click #(put! delete @contact)}"Delete"]]))))
+         [:button {:style {:margin-left "1em"}
+                   :on-click #(put! delete @contact)}"Delete"]]))))
+
+(defn add-contact [data owner]
+  (let [contact-input (om/get-node owner "new-contact")
+        new-contact (parse-contact (.-value contact-input))]
+    (when new-contact
+      (om/transact! data :contacts #(conj % new-contact))
+      (om/set-state! owner :text ""))))
+
+(defn handle-change [e owner {:keys [text]}]
+  (let [value (.. e -target -value)]
+    (if-not (re-find #"[0-9]" value)
+      (om/set-state! owner :text value)
+      (om/set-state! owner :text text))))
 
 (defn contacts-view [data owner]
   (reify
     om/IInitState
       (init-state [_]
-        {:delete (chan)})
-
+        {:delete (chan)
+         :text ""})
     om/IWillMount
     (will-mount [_]
       (let [delete (om/get-state owner :delete)]
@@ -71,17 +121,20 @@
               ;; which returns a lazy seq back into a vector
               (fn [xs] (vec (remove #(= contact %) xs))))
             (recur))))))
-
     om/IRenderState
-    (render-state [_ {:keys [delete]}]
+    (render-state [_ {:keys [delete text] :as state}]
       (html
         [:div
           [:h1 "Contact List"]
+          [:h4 (str "Count: " (count (:contacts @data)))]
           [:ul
             (om/build-all contact-view
                           (:contacts data)
-                          {:init-state {:delete delete}})]]))))
-
-
+                          {:init-state {:delete delete}})]
+          [:div
+           [:input {:type "text" :ref "new-contact" :value text
+                    :on-change #(handle-change % owner state)}]
+           [:button {:style {:margin-left "1em"}
+                     :on-click #(add-contact data owner)} "Add contact"]]]))))
 
 
--- src/hello_cljs/core.cljs
+++ src/hello_cljs/core.cljs
@@ -2,7 +2,7 @@
     (:require
       [om.core :as om :include-macros true]
       [hello-cljs.components
-        :refer [like-seymore my-widget animals contacts-view]]
+        :refer [like-seymore my-widget animals contacts-view registry-view]]
       [sablono.core :refer-macros [html]])
     (:require-macros
       [hello-cljs.utils :refer [logger]]))
@@ -13,6 +13,20 @@
 (defonce app-state
   (atom
     {:likes 0
+     :people
+          [{:type :student :first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+           {:type :student :first "Alyssa" :middle-initial "P" :last "Hacker"
+            :email "aphacker@mit.edu"}
+           {:type :professor :first "Gerald" :middle "Jay" :last "Sussman"
+            :email "metacirc@mit.edu" :classes [:6001 :6946]}
+           {:type :student :first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+           {:type :student :first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+           {:type :professor :first "Hal" :last "Abelson" :email "evalapply@mit.edu"
+            :classes [:6001]}]
+      :classes
+        {:6001 "The Structure and Interpretation of Computer Programs"
+         :6946 "The Structure and Interpretation of Classical Mechanics"
+         :1806 "Linear Algebra"}
      :contacts
         [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
          {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
@@ -30,6 +44,7 @@
         (render [_]
           (html
             [:div.aside-left-menu
+              (om/build registry-view data)
               (om/build contacts-view data)
               (om/build animals data)
               (om/build my-widget data)
