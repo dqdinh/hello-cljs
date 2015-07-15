9c1b66e350dd63ff1055b86d4c9a0554280f2ab2
david dinh <david.dinh@lookout.com>
2015-07-14 22:12:53 -0700

    WIP

--- src/hello_cljs/components.cljs
+++ src/hello_cljs/components.cljs
@@ -1,16 +1,18 @@
 (ns hello-cljs.components
   (:require
     [om.core :as om :include-macros true]
-    [sablono.core :as s]))
+    [sablono.core :refer-macros [html]]))
 
 (defn like-seymore [data owner]
   (reify
     om/IRender
     (render [_]
-      (s/html
+      (html
         [:div
          [:h1 "popular: " (:likes @data)]
          [:div [:a {:href "#"
+                    ;; FIXME: Error: No protocol method ISwap.-swap!
+                    ;; defined for type om.core/MapCursor: [object Object]
                     :onClick #(swap! data assoc :likes inc)}
                 "Thumbs up"]]]))))
 
@@ -18,6 +20,19 @@
   (reify
     om/IRender
     (render [_]
-      (s/html
+      (html
         [:h1 {:style {:color "blue"}} "Holloo"]))))
+
+(defn stripe [text bgc]
+  (let [st {:backgroundColor bgc}]
+    [:li {:style st} text]))
+
+(defn animals [data owner]
+  (om/component
+    (html
+      [:ul
+       ;; Map will pair the each color with an element from the list
+       ;; until it works through the entire list.
+       ;; https://clojuredocs.org/clojure.core/cycle#example-557b10f6e4b01ad59b65f4f2
+       (map stripe (:list data) (cycle ["#ff0" "#f00"]))])))
 
--- src/hello_cljs/core.cljs
+++ src/hello_cljs/core.cljs
@@ -1,26 +1,20 @@
 (ns hello-cljs.core
     (:require
       [om.core :as om :include-macros true]
-      [hello-cljs.components :refer [like-seymore my-widget]]
-      [sablono.core :as s])
+      [hello-cljs.components :refer [like-seymore my-widget animals]]
+      [sablono.core :refer-macros [html]])
     (:require-macros
       [hello-cljs.utils :refer [logger]]))
 
 (enable-console-print!)
 
 ;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:likes 0}))
+(defonce app-state
+  (atom
+    {:likes 0
+     :list ["lion" "zebra" "buffalo" "antelope" "gazelle"]}))
 
-(reset! app-state {:likes 9})
-
-(.log js/console (prn-str @app-state))
-
-
-(defn stripe [text bgc]
-  (let [st {:backgroundColor bgc}]
-    (s/html
-      [:li {:style st}])))
-
+(prn @app-state)
 
 (defn render! []
   (om/root
@@ -28,12 +22,15 @@
       (reify
         om/IRender
         (render [_]
-          (s/html
-            [:nav.aside-left-menu
+          (html
+            [:div.aside-left-menu
+              (om/build animals data)
               (om/build my-widget data)
               (om/build like-seymore data)]))))
     app-state
     {:target (. js/document (getElementById "app"))}))
+
+(render!)
 
 (defn on-js-reload []
   (render!)
